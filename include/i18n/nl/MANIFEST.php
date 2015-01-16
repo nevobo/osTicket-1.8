711dcfd7232f207ba36959b979822aaa05e1a1a7
Thijs-jan Veldhuizen <t.j.veldhuizen@tjveldhuizen.nl>
2015-01-16 12:29:09 +0100

    Nederlandse vertaling, uitgepakt

---
+++ include/i18n/nl/MANIFEST.php
@@ -0,0 +1,8 @@
+<?php return array (
+  'Build-Date' => 'Thu, 08 Jan 15 11:31:41 -0600',
+  'Build-Version' => 'v1.9.5',
+  'Language' => 'nl_NL',
+  'Id' => 'lang:nl',
+  'Last-Revision' => '2015-01-08 12:22-0500',
+  'Version' => 142073,
+);
